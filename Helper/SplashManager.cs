a1376c816708c23bc420c63b3b644e706cf4ca8f
sahilpithiya <sahil.starlinetechno@gmail.com>
2025-09-15 10:43:10 +0530

    Add Form Loader -- Sahil Pithiya

---
+++ Helper/SplashManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace thecalcify.Helper
+{
+    public static class SplashManager
+    {
+        private static SplashForm _splashForm;
+
+        public static void Show(Form parent, string title = "Please Wait", string message = "Loading ...")
+        {
+            if (_splashForm == null || _splashForm.IsDisposed)
+            {
+                _splashForm = new SplashForm(title, message);
+                _splashForm.CenterToParent(parent);
+                _splashForm.Show(parent);
+                _splashForm.TopMost = true;
+                Application.DoEvents();
+            }
+        }
+
+        public static void Update(string title, string message)
+        {
+            if (_splashForm != null && !_splashForm.IsDisposed)
+            {
+                _splashForm.UpdateMessage(title, message);
+            }
+        }
+
+        public static void Hide()
+        {
+            if (_splashForm != null && !_splashForm.IsDisposed)
+            {
+                _splashForm.SafeHide();
+                _splashForm.Close();
+                _splashForm.Dispose();
+                _splashForm = null;
+            }
+        }
+    }
+}
